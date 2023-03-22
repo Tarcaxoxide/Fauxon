2f6e7a5f2c8c54627f7cadef65b69654d13738cd
Tarcaxoxide <loganer@vivaldi.net>
2023-03-22 09:54:02 -0400

    Wed Mar 22 09:54:02 AM EDT 2023

--- Sources/main.cpp
+++ Sources/main.cpp
@@ -1,14 +1,26 @@
 #include <iostream>
+#include <fstream>
 #include <Tokenize.hpp>
 #include <Parse.hpp>
 #include <Evaluate.hpp>
 
-int main(){
-	std::cout<<"user>";
-	for(std::string Line="";std::getline(std::cin,Line);){
-		std::deque<Token::Token> tokens = Tokenize(Line);
+
+int main(int c,char** v){
+	if(c==1){
+		std::cout<<"user>";
+		for(std::string Line="";std::getline(std::cin,Line);){
+			std::deque<Token::Token> tokens = Tokenize(Line);
+			std::shared_ptr<Node::BlockNode> block = Parse_Block(tokens);
+			Evalutate(block);
+			std::cout<<"user>";
+		}
+	}else if(c==2){
+		std::ifstream File(v[1]);
+		std::string Contents="";
+		for(std::string Line="";std::getline(File,Line);){Contents+=Line;}
+		File.close();
+		std::deque<Token::Token> tokens = Tokenize(Contents);
 		std::shared_ptr<Node::BlockNode> block = Parse_Block(tokens);
 		Evalutate(block);
-		std::cout<<"user>";
 	}
 }
