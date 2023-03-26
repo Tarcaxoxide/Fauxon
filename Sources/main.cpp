12186749e0d886b71bd909af735f7dfdf2606b13
Tarcaxoxide <loganer@vivaldi.net>
2023-03-26 17:35:19 -0400

    Sun Mar 26 05:35:19 PM EDT 2023

--- Sources/main.cpp
+++ Sources/main.cpp
@@ -3,7 +3,9 @@
 #include<Interpreter/Interpreter.hpp>
 
 int main(){
+	std::cout<<"user> ";
 	for(std::string Line="";std::getline(std::cin,Line);){
 		std::cout<<Interpreter::Interpreter(Line)<<std::endl;
+		std::cout<<"user> ";
 	}
 }
