4c05bed2423270fb2414b5099f87729c6d9a7225
Tarcaxoxide <loganer@vivaldi.net>
2023-03-26 19:44:59 -0400

    Sun Mar 26 07:44:59 PM EDT 2023

--- Sources/Common/Parser.cpp
+++ Sources/Common/Parser.cpp
@@ -7,56 +7,58 @@ namespace Common{
 			for(int64_t ib=0;ib<input[ia].size();ib++){
 				switch(input[ia][ib][0]){
 					case '+':case '-':case '*':case '/':{
-						std::string leftBuffer=ib-1>-1?(input[ia][ib-1]):(ia-1>-1?input[ia-1][input[ia-1].size()-1]:"...");
+						std::string leftBuffer=ib-1>-1?(input[ia][ib-1]):(ia-1>-1?input[ia-1][input[ia-1].size()-1]:"..");
 						std::string operatorBuffer=input[ia][ib];
-						std::string rightBuffer=ib+1<input[ia].size()?(input[ia][ib+1]):(ia+1<input.size()?input[ia+1][0]:"...");
+						std::string rightBuffer=ib+1<input[ia].size()?(input[ia][ib+1]):(ia+1<input.size()?input[ia+1][0]:"..");
 						std::deque<std::string> Buffer;
+						if(leftBuffer[0]==']'||leftBuffer[0]==')'||leftBuffer[0]=='}'||leftBuffer[0]=='>'){leftBuffer="..";}
+						if(rightBuffer[0]=='['||rightBuffer[0]=='('||rightBuffer[0]=='{'||rightBuffer[0]=='<'){rightBuffer="..";}
 						Buffer.push_back(leftBuffer);
 						Buffer.push_back(operatorBuffer);
 						Buffer.push_back(rightBuffer);
 						line.push_back(Buffer);
-						ia++;ib++;
+						if(ia<input.size()-1&&ib<input[ia].size()){ia++;ib++;}
 					}break;
 					case '[':{
 						std::deque<std::string> Buffer;
 						while(ia<input.size()&&ib<input[ia].size()&&input[ia][ib][0] != ']'){
-							Buffer.push_back(input[ia][ib]);
-							if(++ib>input[ia].size()){ib=0;ia++;}
+							Buffer.push_back(input[ia][ib++]);
+							if(ib>input[ia].size()){ib=0;ia++;}
 						}
-						if(ia<input.size()){Buffer.push_back(input[ia][ib]);}
+						Buffer.push_back(input[ia][ib]);
 						line.push_back(Buffer);
 					}break;
 					case '(':{
 						std::deque<std::string> Buffer;
 						while(ia<input.size()&&ib<input[ia].size()&&input[ia][ib][0] != ')'){
-							Buffer.push_back(input[ia][ib]);
-							if(++ib>input[ia].size()){ib=0;ia++;}
+							Buffer.push_back(input[ia][ib++]);
+							if(ib>input[ia].size()){ib=0;ia++;}
 						}
-						if(ia<input.size()){Buffer.push_back(input[ia][ib]);}
+						Buffer.push_back(input[ia][ib]);
 						line.push_back(Buffer);
 					}break;
 					case '{':{
 						std::deque<std::string> Buffer;
 						while(ia<input.size()&&ib<input[ia].size()&&input[ia][ib][0] != '}'){
-							Buffer.push_back(input[ia][ib]);
-							if(++ib>input[ia].size()){ib=0;ia++;}
+							Buffer.push_back(input[ia][ib++]);
+							if(ib>input[ia].size()){ib=0;ia++;}
 						}
-						if(ia<input.size()){Buffer.push_back(input[ia][ib]);}
+						Buffer.push_back(input[ia][ib]);
 						line.push_back(Buffer);
 					}break;
 					case '<':{
 						std::deque<std::string> Buffer;
 						while(ia<input.size()&&ib<input[ia].size()&&input[ia][ib][0] != '>'){
-							Buffer.push_back(input[ia][ib]);
-							if(++ib>input[ia].size()){ib=0;ia++;}
+							Buffer.push_back(input[ia][ib++]);
+							if(ib>input[ia].size()){ib=0;ia++;}
 						}
-						if(ia<input.size()){Buffer.push_back(input[ia][ib]);}
+						Buffer.push_back(input[ia][ib]);
 						line.push_back(Buffer);
 					}break;
 					default:{
-						std::deque<std::string> Buffer;
-						Buffer.push_back(input[ia][ib]);
-						line.push_back(Buffer);
+						//std::deque<std::string> Buffer;
+						//Buffer.push_back(input[ia][ib]);
+						//line.push_back(Buffer);
 					}break;
 				}
 			}
