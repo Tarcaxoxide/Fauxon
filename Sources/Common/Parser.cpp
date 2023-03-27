11056b2037d04c75689f564ef53d932d26162db1
Tarcaxoxide <loganer@vivaldi.net>
2023-03-27 01:38:43 -0400

    Mon Mar 27 01:38:43 AM EDT 2023

--- Sources/Common/Parser.cpp
+++ Sources/Common/Parser.cpp
@@ -7,19 +7,6 @@ namespace Common{
 		for(int64_t ia=0;ia<input.size();ia++){
 			for(int64_t ib=0;ib<input[ia].size();ib++){
 				switch(input[ia][ib][0]){
-					case '+':case '-':case '*':case '/':{
-						std::string leftBuffer=ib-1>-1?(input[ia][ib-1]):(ia-1>-1?input[ia-1][input[ia-1].size()-1]:"..");
-						std::string operatorBuffer=input[ia][ib];
-						std::string rightBuffer=ib+1<input[ia].size()?(input[ia][ib+1]):(ia+1<input.size()?input[ia+1][0]:"..");
-						std::deque<std::string> Buffer;
-						if(leftBuffer[0]==']'||leftBuffer[0]==')'||leftBuffer[0]=='}'||leftBuffer[0]=='>'){leftBuffer="..";}
-						if(rightBuffer[0]=='['||rightBuffer[0]=='('||rightBuffer[0]=='{'||rightBuffer[0]=='<'){rightBuffer="..";}
-						Buffer.push_back(leftBuffer);
-						Buffer.push_back(operatorBuffer);
-						Buffer.push_back(rightBuffer);
-						line.push_back(Buffer);
-						if(ia<input.size()-1&&ib<input[ia].size()){ib++;}
-					}break;
 					case '[':{
 						std::deque<std::string> Buffer;
 						do{
@@ -68,7 +55,11 @@ namespace Common{
 						Buffer.push_back(input[ia][ib++]);
 						line.push_back(Buffer);
 					}break;
-					default:{}break;
+					default:{
+						std::deque<std::string> Buffer;
+						Buffer.push_back(input[ia][ib]);
+						line.push_back(Buffer);
+					}break;
 				}
 			}
 		}
--- Sources/Interpreter/Interpreter.cpp
+++ Sources/Interpreter/Interpreter.cpp
@@ -3,18 +3,179 @@
 #include<Common/Parser.hpp>
 #include<deque>
 
+
+#include<iostream>
+#include<cassert>
+
+std::deque<std::deque<std::string>> stack;
+
+
+std::deque<std::string> Calculate_ArrayOnArray(const std::deque<std::string>& A,const std::deque<std::string>& B,const char op){
+	std::deque<std::string> Result;
+	Result.push_back("[");
+	switch(op){
+		case '+':{
+			for(size_t i=0;(i<A.size()||i<B.size());i++){
+				if(A[i][0]=='['||A[i][0]==']')continue;
+				if(B[i][0]=='['||B[i][0]==']')continue;
+				if(!(i<A.size())){
+					Result.push_back(B[i]);
+				}else if(!(i<B.size())){
+					Result.push_back(A[i]);
+				}else{
+					Result.push_back(std::to_string(std::stoll(A[i])+std::stoll(B[i])));
+				}
+			}
+		}break;
+		case '-':{
+			for(size_t i=0;(i<A.size()||i<B.size());i++){
+				if(A[i][0]=='['||A[i][0]==']')continue;
+				if(B[i][0]=='['||B[i][0]==']')continue;
+				if(!(i<A.size())){
+					Result.push_back(B[i]);
+				}else if(!(i<B.size())){
+					Result.push_back(A[i]);
+				}else{
+					Result.push_back(std::to_string(std::stoll(A[i])-std::stoll(B[i])));
+				}
+			}
+		}break;
+		case '*':{
+			for(size_t i=0;(i<A.size()||i<B.size());i++){
+				if(A[i][0]=='['||A[i][0]==']')continue;
+				if(B[i][0]=='['||B[i][0]==']')continue;
+				if(!(i<A.size())){
+					Result.push_back(B[i]);
+				}else if(!(i<B.size())){
+					Result.push_back(A[i]);
+				}else{
+					Result.push_back(std::to_string(std::stoll(A[i])*std::stoll(B[i])));
+				}
+			}
+		}break;
+		case '/':{
+			for(size_t i=0;(i<A.size()||i<B.size());i++){
+				if(A[i][0]=='['||A[i][0]==']')continue;
+				if(B[i][0]=='['||B[i][0]==']')continue;
+				if(!(i<A.size())){
+					Result.push_back(B[i]);
+				}else if(!(i<B.size())){
+					Result.push_back(A[i]);
+				}else{
+					Result.push_back(std::to_string(std::stoll(A[i])/std::stoll(B[i])));
+				}
+			}
+		}break;
+	}
+	Result.push_back("]");
+	return Result;
+}
+std::deque<std::string> Calculate_ArrayOnList(const std::deque<std::string>& A,const std::deque<std::string>& B,const char op){
+	std::deque<std::string> Result;
+	assert(false);//unimplemented
+	return Result;
+}
+std::deque<std::string> Calculate_ArrayOnSingle(const std::deque<std::string>& A,const std::deque<std::string>& B,const char op){
+	std::deque<std::string> Result;
+	assert(false);//unimplemented
+	return Result;
+}
+
+std::deque<std::string> Calculate_ListOnArray(const std::deque<std::string>& A,const std::deque<std::string>& B,const char op){
+	std::deque<std::string> Result;
+	assert(false);//unimplemented
+	return Result;
+}
+std::deque<std::string> Calculate_ListOnList(const std::deque<std::string>& A,const std::deque<std::string>& B,const char op){
+	std::deque<std::string> Result;
+	assert(false);//unimplemented
+	return Result;
+}
+std::deque<std::string> Calculate_ListOnSingle(const std::deque<std::string>& A,const std::deque<std::string>& B,const char op){
+	std::deque<std::string> Result;
+	assert(false);//unimplemented
+	return Result;
+}
+
+std::deque<std::string> Calculate_SingleOnList(const std::deque<std::string>& A,const std::deque<std::string>& B,const char op){
+	std::deque<std::string> Result;
+	assert(false);//unimplemented
+	return Result;
+}
+std::deque<std::string> Calculate_SingleOnArray(const std::deque<std::string>& A,const std::deque<std::string>& B,const char op){
+	std::deque<std::string> Result;
+	assert(false);//unimplemented
+	return Result;
+}
+std::deque<std::string> Calculate_SingleOnSingle(const std::deque<std::string>& A,const std::deque<std::string>& B,const char op){
+	std::deque<std::string> Result;
+	assert(false);//unimplemented
+	return Result;
+}
+
+
 namespace Interpreter{
 	std::string Interpreter(std::string input){
 		std::deque<std::deque<std::string>> tokens = Common::Tokenizer(input);
 		std::deque<std::deque<std::string>> parsed = Common::Parser(tokens);
 		std::string Result="";
 		for(auto token:parsed){
-			for(auto part:token){
-				Result+=part+" ";
+			switch(token[0][0]){
+				case '?':{
+					assert(stack.size()>0);
+					for(auto stackElement:stack){
+						for(auto stackItem:stackElement){
+							std::cout<<stackItem<<" ";
+						}
+					}
+					stack.clear();
+				}break;
+				case '+':{
+					assert(stack.size()>0);
+					std::deque<std::string> local_stack_B = stack.back();stack.pop_back();
+					assert(stack.size()>0);
+					std::deque<std::string> local_stack_A = stack.back();stack.pop_back();
+					std::deque<std::string> local_stack_Results;
+					switch(local_stack_A[0][0]){
+						case '<'/*list*/:{
+							switch(local_stack_B[0][0]){
+								case '<'/*list*/:{local_stack_Results = Calculate_ListOnList(local_stack_A,local_stack_B,'+');}break;
+								case '['/*array*/:{local_stack_Results = Calculate_ListOnArray(local_stack_A,local_stack_B,'+');}break;
+								case '{'/*body*/:{assert(false);/*you can't do math with this*/}break;
+								case '('/*argument list*/:{assert(false);/*you can't do math with this*/}break;
+								default/*single number, hopefully*/:{local_stack_Results = Calculate_ListOnSingle(local_stack_A,local_stack_B,'+');}break;
+							}
+						}break;
+						case '['/*array*/:{
+							switch(local_stack_B[0][0]){
+								case '<'/*list*/:{local_stack_Results = Calculate_ArrayOnList(local_stack_A,local_stack_B,'+');}break;
+								case '['/*array*/:{local_stack_Results = Calculate_ArrayOnArray(local_stack_A,local_stack_B,'+');}break;
+								case '{'/*body*/:{assert(false);/*you can't do math with this*/}break;
+								case '('/*argument list*/:{assert(false);/*you can't do math with this*/}break;
+								default/*single number, hopefully*/:{local_stack_Results = Calculate_ArrayOnSingle(local_stack_A,local_stack_B,'+');}break;
+							}
+						}break;
+						case '{'/*body*/:{assert(false);/*you can't do math with this*/}break;
+						case '('/*argument list*/:{assert(false);/*you can't do math with this*/}break;
+						default/*single number, hopefully*/:{
+							switch(local_stack_B[0][0]){
+								case '<'/*list*/:{local_stack_Results = Calculate_SingleOnList(local_stack_A,local_stack_B,'+');}break;
+								case '['/*array*/:{local_stack_Results = Calculate_SingleOnArray(local_stack_A,local_stack_B,'+');}break;
+								case '{'/*body*/:{assert(false);/*you can't do math with this*/}break;
+								case '('/*argument list*/:{assert(false);/*you can't do math with this*/}break;
+								default/*single number, hopefully*/:{local_stack_Results = Calculate_SingleOnSingle(local_stack_A,local_stack_B,'+');}break;
+							}
+						}break;
+					}
+					stack.push_back(local_stack_Results);
+				}break;
+				default:{
+					stack.push_back(token);
+				}break;
 			}
-			Result+="\n";
 		}
-		Result.erase(Result.length()-1,1);
+
+		//Result.erase(Result.length()-1,1);
 		return Result;
 	}
 };
